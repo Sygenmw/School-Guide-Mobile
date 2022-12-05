77041e37ffb09022fb9966002e68395098150eea
xebastian <ernesty533k@gmail.com>
2022-12-05 09:45:35 +0200

    Fixed Location handling issues

--- lib/controllers/permission_controller.dart
+++ lib/controllers/permission_controller.dart
@@ -1,16 +1,34 @@
-import 'package:permission_handler/permission_handler.dart';
+import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart' as permision;
+import 'package:school_guide/style/app_styles.dart';
+import 'package:school_guide/views/widgets/custom_snackbar.dart';
+import 'package:app_settings/app_settings.dart';
 
 class PermissionHandler {
-  var status = Permission.location.status;
   static askLocationPermission() async {
-    // request location permissions
-    if (await Permission.location.status.isGranted) {
-    } else if (await Permission.location.status.isDenied) {
-      Permission.location.request();
-    } else if (await Permission.location.status.isPermanentlyDenied) {
-      openAppSettings();
-    } else {
-      Permission.location.request();
+    permision.PermissionStatus locationPermissionStatus = await permision.Permission.location.request();
+    Location location = Location();
+    bool ison = await location.serviceEnabled();
+    if (locationPermissionStatus.isDenied) {
+      permision.Permission.location.request();
+    }
+    if (locationPermissionStatus.isPermanentlyDenied) {
+      permision.openAppSettings();
+    }
+    if (!ison) {
+      bool isturnedon = await location.requestService();
+      if (isturnedon) {
+        //  OKAY CONTINUE
+        if (locationPermissionStatus.isDenied) {
+          permision.Permission.location.request();
+        }
+        if (locationPermissionStatus.isPermanentlyDenied) {
+          permision.openAppSettings();
+        }
+      } else {
+        CustomSnackBar.showSnackBar(title: 'App Needs Location Permission', color: AppColors.errorColor, message: 'To access nearby schools, App needs to access your Location. Please Allow.');
+        AppSettings.openLocationSettings();
+      }
     }
   }
 }
